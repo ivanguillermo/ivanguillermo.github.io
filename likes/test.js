40da6cc5ab732529d17a836b8e764b2df064402a
Ivan Lopez Pedreañez <ivanglopezp@gmail.com>
2022-01-18 01:28:07 -0300

    Add files via upload

---
+++ likes/test.js
@@ -0,0 +1,128 @@
+
+	  
+	  var psyp_epi =[
+	  ['Psycho Pass','psy_'],
+	  ['10.1 The One After Joey and Rachel Kiss','219','Sep 25, 2003','series/Helluva_Boss/Helluva_Boss_0.mp4'],];
+
+	  var sel_epi=[['Serial Experiment Lain','sel_'],
+	  ['1 Weird','Raro','Jul 06 1998','series/Helluva_Boss/Helluva_Boss_0.mp4'],
+	  ['2 Girls','Chicas','Jul 06 1998','series/Helluva_Boss/Helluva_Boss_0.mp4'],
+	  ['3 Psyche','Psyche','Jul 06 1998','series/Helluva_Boss/Helluva_Boss_0.mp4'],
+	  ['4 Religion','Religion','Jul 06 1998','series/Helluva_Boss/Helluva_Boss_0.mp4'],
+	  ['5 Distorsion','Distorsion','Jul 06 1998','series/Helluva_Boss/Helluva_Boss_0.mp4'],
+	  ['6 Kids','Chicos','Jul 06 1998','series/Helluva_Boss/Helluva_Boss_0.mp4'],
+	  ['7 Society','Sociedad','Jul 06 1998','series/Helluva_Boss/Helluva_Boss_0.mp4'],
+	  ['8 Rumors','Rumores','Ago 24 1998','series/Helluva_Boss/Helluva_Boss_0.mp4'],
+	  ['9 Protocols','Protocolos','Ago 31 1998','series/Helluva_Boss/Helluva_Boss_0.mp4'],
+	  ['10 Love','Amor','Sep 07 1998','series/Helluva_Boss/Helluva_Boss_0.mp4'],
+	  ['11 Infornography','Infornografia','Sep 14 1998','series/Helluva_Boss/Helluva_Boss_0.mp4'],
+	  ['12 Landscape','Paisaje','Sep 21 1998','series/Helluva_Boss/Helluva_Boss_0.mp4'],
+	  ['13 Ego','Ego','Sep 28 1998','series/Helluva_Boss/Helluva_Boss_0.mp4'],
+	            
+	  ];
+	  var fri_epi=[['F.R.I.E.N.D.S.','Fri_'],
+	            ['10.1 The One After Joey and Rachel Kiss','219','Sep 25, 2003','series/Helluva_Boss/Helluva_Boss_0.mp4'],
+	            ['10.2 The One Where Ross Is Fine','220','Sep 25, 2003','series/friends/friends_10_02.mp4'],
+	            ['10.3 The One with Ross Tan','221','Sep 25, 2003','series/friends/friends_10_03.mp4'],
+	            ['10.4 The One with the Cake','222','Sep 25, 2003','series/friends/friends_10_04.mp4'],
+	            ['10.5 The One Where Rachels Sister Baby-Sits','223','Sep 25, 2003','series/friends/friends_10_05.mp4'],
+	            ['10.6 The One with Ross Grant','224','Sep 25, 2003','series/friends/friends_10_06.mp4'],
+	            ['10.7 The One with the Home Study','225','Sep 25, 2003','series/friends/friends_10_07.mp4'],
+	            ['10.8 The One with the Late Thanksgiving','226','Sep 25, 2003','series/friends/friends_10_08.mp4'],
+	            ['10.9 The One with the Birth Mother','227','Sep 25, 2003','series/friends/friends_10_09.mp4'],
+	            ['10.10 The One Where Chandler Gets Caught','228','Sep 25, 2003','series/friends/friends_10_10.mp4'],
+	            ['10.11 The One Where the Stripper Cries','229','Sep 25, 2003','series/friends/friends_10_11.mp4'],
+	            ['10.12 The One with Phoebes Wedding','230','Feb 12, 2004','series/friends/friends_10_12.mp4'],
+	            ['10.13 The One Where Joey Speaks French','231','Feb 19, 2004','series/friends/friends_10_13.mp4'],
+	            ['10.14 The One with Princess Consuela','232','Feb 26, 2004','series/friends/friends_10_14.mp4'],
+	            ['10.15 The One Where Estelle Dies','233','Abr 22, 2004','series/friends/friends_10_15.mp4'],
+	            ['10.16 The One with Rachels Going Away Party','234','Abr 29, 2004','series/friends/friends_10_16.mp4'],
+	            ['10.17 The Last One','235','May 06, 2004','series/friends/friends_10_17.mp4'],
+	            ];
+
+
+
+	  var hb_epi=[['Helluva Boss','HB_'],
+	            ['Helluva Boss pilot0','pilot','NaN','series/Helluva_Boss/Helluva_Boss_0.mp4'],
+	            ['Helluva Boss 1','1','NaN','series/Helluva_Boss/Helluva_Boss_1.mp4'],
+	            ['Helluva Boss 2','2','NaN','series/Helluva_Boss/Helluva_Boss_2.mp4'],
+	            ['Helluva Boss 3','3','NaN','series/Helluva_Boss/Helluva_Boss_3.mp4'],
+	            ['Helluva Boss 4','4','NaN','series/Helluva_Boss/Helluva_Boss_4.mp4'],
+	            ['Helluva Boss 5','5','NaN','series/Helluva_Boss/Helluva_Boss_5.mp4'],
+	            ['Helluva Boss 6','6','NaN','series/Helluva_Boss/Helluva_Boss_6.mp4'],
+	            ];
+
+	  var dn_epi = [['Death Note','DN_'],
+	            ['1. Renacimiento','Shinsei (新生)','Oct 03, 2006','https://www.dropbox.com/s/xy14s0i0zpm95jy/Death%20Note%201%20Sub%20Esp.mp4.mp4?raw=1'],
+	            ['2. Confrontación','Taiketsu (対决) ','Oct 10, 2006','https://www.dropbox.com/s/xy14s0i0zpm95jy/Death%20Note%202%20Sub%20Esp.mp4.mp4?raw=1'],
+	            ['3. Negociaciones ','Torihiki (取引)','Oct 14, 2006','https://www.dropbox.com/s/xy14s0i0zpm95jy/Death%20Note%203%20Sub%20Esp.mp4.mp4?raw=1'],
+	            ['4. Persecución','Tsuiseki (追跡)','Oct, 2006','https://www.dropbox.com/s/xy14s0i0zpm95jy/Death%20Note%204%20Sub%20Esp.mp4.mp4?raw=1'],
+	            ['5. Estrategia','Kakehiki (駆引)','Octubre 31, 2006','https://www.dropbox.com/s/xy14s0i0zpm95jy/Death%20Note%205%20Sub%20Esp.mp4.mp4?raw=1'],
+	            ['6. Florecer','Hokorobi (綻び) ','Nov 7, 2006','https://www.dropbox.com/s/xy14s0i0zpm95jy/Death%20Note%206%20Sub%20Esp.mp4.mp4?raw=1'],
+	            ['7. Nublado','Donten (曇天) ','Nov 14, 2006','https://www.dropbox.com/s/xy14s0i0zpm95jy/Death%20Note%207%20Sub%20Esp.mp4.mp4?raw=1'],
+	            ['8. Mirada','Mesen (目線) ','Nov 21, 2006','https://www.dropbox.com/s/xy14s0i0zpm95jy/Death%20Note%208%20Sub%20Esp.mp4.mp4?raw=1'],
+	            ['9. Contacto','Sesshoku (接触) ','Nov 28, 2006','https://www.dropbox.com/s/xy14s0i0zpm95jy/Death%20Note%209%20Sub%20Esp.mp4.mp4?raw=1'],
+	            ['10. Sospechas','Giwaku (疑惑) ','Dic 5 , 2006','https://www.dropbox.com/s/xy14s0i0zpm95jy/Death%20Note%210%20Sub%20Esp.mp4.mp4?raw=1'],
+	            ['11. Asalto','Totsunyū (突入) ','Dic 12, 2006','https://www.dropbox.com/s/xy14s0i0zpm95jy/Death%20Note%211%20Sub%20Esp.mp4.mp4?raw=1'],
+	            ['12. Enamoramiento','Koigokoro (恋心) ','Dic 19, 2006','https://www.dropbox.com/s/xy14s0i0zpm95jy/Death%20Note%212%20Sub%20Esp.mp4.mp4?raw=1'],
+	            ['13. Confesión',' Kokuhaku (告白)','Dic 26, 2006','https://www.dropbox.com/s/xy14s0i0zpm95jy/Death%20Note%213%20Sub%20Esp.mp4.mp4?raw=1'],
+	            ['14. Amigo','Tomodachi (友達) ','Ene 2, 2007','https://www.dropbox.com/s/xy14s0i0zpm95jy/Death%20Note%214%20Sub%20Esp.mp4.mp4?raw=1'],
+	            ['15. Apuesta','Kake (賭け) ','Oct, 2007','https://www.dropbox.com/s/xy14s0i0zpm95jy/Death%20Note%215%20Sub%20Esp.mp4.mp4?raw=1'],
+	            ['16. Decisión','Ketsudan (決断) ','Oct, 2007','https://www.dropbox.com/s/xy14s0i0zpm95jy/Death%20Note%216%20Sub%20Esp.mp4.mp4?raw=1'],
+	            ['17. Ejecución','Shikkō (執行) ','Oct, 2007','https://www.dropbox.com/s/xy14s0i0zpm95jy/Death%20Note%217%20Sub%20Esp.mp4.mp4?raw=1'],
+	            ['18. Compañeros','Nakama (仲間) ','Oct, 2007','https://www.dropbox.com/s/xy14s0i0zpm95jy/Death%20Note%218%20Sub%20Esp.mp4.mp4?raw=1'],
+	            ['19. Matsuda','Matsuda (松田) ','Oct, 2007','https://www.dropbox.com/s/xy14s0i0zpm95jy/Death%20Note%219%20Sub%20Esp.mp4.mp4?raw=1'],
+	            ['20. Improvisación','Kosoku (姑息) ','Oct, 2007','https://www.dropbox.com/s/xy14s0i0zpm95jy/Death%20Note%220%20Sub%20Esp.mp4.mp4?raw=1'],
+	            ['21. Actividad','Katsuyaku (活躍) ','Oct, 2007','https://www.dropbox.com/s/xy14s0i0zpm95jy/Death%20Note%221%20Sub%20Esp.mp4.mp4?raw=1'],
+	            ['22. Inducción','Yūdō (誘導) ','Oct, 2007','https://www.dropbox.com/s/xy14s0i0zpm95jy/Death%20Note%222%20Sub%20Esp.mp4.mp4?raw=1'],
+	            ['23. Frenesí','Kyōsō (狂騒) ','Oct, 2007','https://www.dropbox.com/s/xy14s0i0zpm95jy/Death%20Note%223%20Sub%20Esp.mp4.mp4?raw=1'],
+	            ['24. Resurrección','Fukkatsu (復活) ','Oct, 2007','https://www.dropbox.com/s/xy14s0i0zpm95jy/Death%20Note%224%20Sub%20Esp.mp4.mp4?raw=1'],
+	            ['25. Silencio','Chinmoku (沈黙)','Oct, 2007','https://www.dropbox.com/s/xy14s0i0zpm95jy/Death%20Note%225%20Sub%20Esp.mp4.mp4?raw=1'],
+	            ['26. Regeneración','Saisei (再生) ','Oct, 2007','https://www.dropbox.com/s/xy14s0i0zpm95jy/Death%20Note%226%20Sub%20Esp.mp4.mp4?raw=1'],
+	            ['27. Secuestro','Yūkai (誘拐) ','Oct, 2007','https://www.dropbox.com/s/xy14s0i0zpm95jy/Death%20Note%227%20Sub%20Esp.mp4.mp4?raw=1'],
+	            ['28. Impaciencia','Shōsō (焦燥) ','Oct, 2007','https://www.dropbox.com/s/xy14s0i0zpm95jy/Death%20Note%228%20Sub%20Esp.mp4.mp4?raw=1'],
+	            ['29. Padre','Chichioya (父親) ','Oct, 2007','https://www.dropbox.com/s/xy14s0i0zpm95jy/Death%20Note%229%20Sub%20Esp.mp4.mp4?raw=1'],
+	            ['30. Justicia','Seigi (正義) ','Oct, 2007','https://www.dropbox.com/s/xy14s0i0zpm95jy/Death%20Note%230%20Sub%20Esp.mp4.mp4?raw=1'],
+	            ['31. Traspaso','Ijō (移譲) ','Oct, 2007','https://www.dropbox.com/s/xy14s0i0zpm95jy/Death%20Note%231%20Sub%20Esp.mp4.mp4?raw=1'],
+	            ['32. Selección','Sentaku (選択) ','Oct, 2007','https://www.dropbox.com/s/xy14s0i0zpm95jy/Death%20Note%232%20Sub%20Esp.mp4.mp4?raw=1'],
+	            ['33. Desprecio','Chōshō (嘲笑) ','Oct, 2007','https://www.dropbox.com/s/xy14s0i0zpm95jy/Death%20Note%233%20Sub%20Esp.mp4.mp4?raw=1'],
+	            ['34. Acecho','Koshi (虎視) ','Oct, 2007','https://www.dropbox.com/s/xy14s0i0zpm95jy/Death%20Note%234%20Sub%20Esp.mp4.mp4?raw=1'],
+	            ['35. Malicia','Satsui (殺意) ','Oct, 2007','https://www.dropbox.com/s/xy14s0i0zpm95jy/Death%20Note%235%20Sub%20Esp.mp4.mp4?raw=1'],
+	            ['36. 28 de enero','28 (1.28) ','Oct, 2007','https://www.dropbox.com/s/xy14s0i0zpm95jy/Death%20Note%236%20Sub%20Esp.mp4.mp4?raw=1'],
+	            ['37. Nuevo Mundo','Shinsekai (新世界) ','Junio 26, 2007','https://www.dropbox.com/s/xy14s0i0zpm95jy/Death%20Note%237%20Sub%20Esp.mp4.mp4?raw=1']
+	          ];
+	    var series_epi=[
+	    [psyp_epi, dn_epi, sel_epi, fri_epi, hb_epi],
+	    ['Psycho_Pass','Death_Note','SEL','friends','Helluva_Boss']
+	    ];
+
+	    var epi_tit = document.getElementById('epi_ser_name');
+		var epi= document.getElementById('episodios');
+	  
+
+		function print_epi(x){		
+
+		const player = document.getElementById('player_video_p');
+		const fgh = series_epi[1][x].toLowerCase();
+				
+		player.poster= 'series/'+ fgh + '.png';
+
+	    var k=series_epi[0][x];
+	    epi_tit.innerHTML=k[0][0];
+	  	for (i = 1; i < k.length; i++) 
+	    {epi.innerHTML += 
+	              `<tr>                    
+	                  <td>
+	                    <div class='preview' style="background:url('series/${series_epi[1][x]}/${k[0][1]}${i}.png')">
+	                      <source src="${k[i][3]}" type="video/mp4">
+	                    </div>
+	                    <div>                       
+	                        <i data-label="epi_name">${k[i][0]} </i><br/>
+	                        <i data-label="epi_a">${k[i][1]} </i><br/>
+	                        <i data-label="epi_b">${k[i][2]}</i><br/>
+	                    </div>
+	                  </td>
+	              </tr>`;}
+	  };
+	  print_epi(4);
+  
