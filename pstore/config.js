36c7a5f5a5e7e13aad181ea99968aa3ec8c05e68
Ivan Guillermo Lopez <ivanglopezp@gmail.com>
2026-08-05 12:59:16 -0400

    Create config.js

---
+++ pstore/config.js
@@ -0,0 +1,34 @@
+// ==========================================
+// CONFIGURACIÓN BASE LOCAL Y DE RESPALDO
+// ==========================================
+
+const CONFIG_PSTORE = {
+  // 1. Identidad y Contacto de la Tienda
+  nombreTienda: "Pstore",
+  urlLogo: "assets/pstore.jpg",
+  numeroWhatsapp: "+584126216661",
+  mensajeBienvenida: "¡Bienvenidos a Pstore!",
+  urlPdfCatalogo: "https://drive.google.com/file/d/1-nmOf4b58Y3ioW4Cnym-uev9I5j8h72C/view",
+  
+  // Redes Sociales (Opcionales)
+  linkInstagram: "https://www.instagram.com/pstorevzla",
+  linkFacebook: "https://www.facebook.com/people/Pstore-Variedades/100069365366035/",
+
+  // 2. Moneda Alternativa
+  // Importante: No colocamos tasa por defecto (null) para no mostrar valores desactualizados.
+  simboloMonedaAlt: "Bs.",
+
+  // 3. Enlace de publicación CSV de tu pestaña 'Configuracion' en Google Sheets
+  urlSheetConfig: "https://docs.google.com/spreadsheets/d/e/2PACX-1vQ_D4Cym7p0ATsh5UCG2Q3kbvhy5WuMPx0Q8gCfdz_l9IDoaCb4jn1T8zQ9YKCCvt-0GA0vkDrwKXX2/pub?gid=1458625703&single=true&output=csv",
+
+  // 4. Estilos CSS por defecto (Variables :root)
+  estilosCSS: {
+    "--brand-black": "#111111",
+    "--brand-gold": "#d4af37",
+    "--bg-main": "#0d0d0d",
+    "--bg-card": "#1e1e1e",
+    "--text-primary": "#ffffff",
+    "--text-secondary": "#a0a0a0",
+    "--border-color": "#333333"
+  }
+};
